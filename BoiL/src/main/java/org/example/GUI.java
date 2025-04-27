40b995f3eb27f3d4da640bf0dede1caac1de5a7c
stundeto <147718971+stundeto@users.noreply.github.com>
2025-04-27 16:08:51 +0200

    Basic GUI
    Created basic gui

---
+++ BoiL/src/main/java/org/example/GUI.java
@@ -0,0 +1,49 @@
+package org.example;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class GUI extends JFrame{
+    private JPanel contentPane;
+    private JButton chooseFile;
+    private JTextField displaySelected;
+    private JButton runbutton;
+    NodeManager nodeManager;
+    String selectedFilePath;
+
+    GUI() {
+        setContentPane(contentPane);
+        setTitle("Project Management Tool");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        runbutton.setEnabled(false);
+        nodeManager = new NodeManager();
+        chooseFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    selectedFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+                    displaySelected.setText(selectedFilePath);
+                    runbutton.setEnabled(true);
+                }
+            }
+        });
+        runbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                nodeManager.loadNodes(selectedFilePath);
+                nodeManager.connectNodes(selectedFilePath);
+
+                List<Node> allNodes = nodeManager.getNodes();
+                Node specificNode = nodeManager.getNodeByName("A");
+                System.out.println("Specific Node: " + specificNode);
+            }
+        });
+    }
+}
--- BoiL/src/main/java/org/example/Main.java
+++ BoiL/src/main/java/org/example/Main.java
@@ -1,35 +1,11 @@
 package org.example;
 
+import javax.swing.*;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
-        //System.out.printf("Hello and welcome!");
-
-        //for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            //System.out.println("i = " + i);
-        //}
-        /*
-        //quick test to verify if loading works (it sure does like it
-        NodeManager manager = new NodeManager();
-        String filePath = "BoiL/src/main/java/org/example/nodes.csv";
-
-        // Load nodes and their connections
-        manager.loadNodes(filePath);
-        manager.connectNodes(filePath);
-
-        // Access nodes
-        List<Node> allNodes = manager.getNodes();
-        Node specificNode = manager.getNodeByName("A");
-        System.out.println("Specific Node: " + specificNode);
-        */
+        JFrame frame = new GUI();
 
     }
 }
